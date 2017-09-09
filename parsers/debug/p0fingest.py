37b03600cb7404210b00d7c4e61470f6ba9aa042
Michael Vaouli <mykelxknight@Michaels-Mini.home>
2017-09-09 07:15:35 -0700

    Pub/Priv Scan Added

--- parsers/debug/p0fingest.py
+++ parsers/debug/p0fingest.py
@@ -1,35 +1,26 @@
 import pandas as pd
 import logging, os
+import ipaddress
 #from pprint import pprint
 
-log = logging.getLogger(__name__)
 
-class p0f_ingestion:
-"""Parse p0f related data
 
-  Attributes:
-  directory_to_parse: path to p0f data
-  move_files_after_parsing: set to false if we want to retain files after parsing
-  delog: to remove files in set directory
-  inFile: to read data in log"""
+pof_df = pd.read_csv('data/p0f/p0f.txt',sep='|',header=0, engine='python')
+pof_df.columns = ['time','client_ip','server_ip','d','connection','mtu','g','raw_ip']
+pof_df['client_ip'] = pof_df['client_ip'].map(lambda x: str(x)[4:])
+pof_df['server_ip'] = pof_df['server_ip'].map(lambda x: str(x)[4:])
+pof_df['mtu'] = pof_df['mtu'].map(lambda x:  x.lstrip('raw_mtu=langEnglishnonefreqdistHz').rstrip('=Hz'))
+pof_df['time'] = pof_df['time'].map(lambda x:  x.lstrip('').rstrip('mod=mtu,cli=mod=syn+ackmod=uptimemod=httpreqmod= httpmod=host chang'))
+pof_df['cip'] = pof_df['client_ip'].apply(lambda x: x.split('/')[0])
+pof_df['c_public'] = pof_df['cip'].apply(lambda x: ipaddress.ip_address(x).is_global)
+pof_df['c_private'] = pof_df['cip'].apply(lambda x: ipaddress.ip_address(x).is_private)
+pof_df['sip'] = pof_df['server_ip'].apply(lambda x: x.split('/')[0])
+pof_df['s_private'] = pof_df['sip'].apply(lambda x: ipaddress.ip_address(x).is_private)
+sprivate_pof_df = pof_df.loc[pof_df['s_private'] == True]
+spublic_pof_df = pof_df.loc[pof_df['s_private'] == False]
 
-    def __init__(self, directory_to_parse, move_files_after_parsing = true):
-        self.directory = directory_to_parse
-        self.flag = move_files_after_parsing
 
-    def move_data(delog):
-#excute an os command to read all files in directory and delete
-        os.system('rm '+delog)
+print (pof_df)
 
-    def p0f_parse(inFile):
-        df2 = pd.read_csv( inFile,sep='|',header=0, engine='python')
-        df2.columns = ['time','client_ip','server_ip','d','connection','mtu','g','raw_ip']
-        df2['client_ip'] = df2['client_ip'].map(lambda x: str(x)[4:])
-        df2['server_ip'] = df2['server_ip'].map(lambda x: str(x)[4:])
-        df2['mtu'] = df2['mtu'].map(lambda x:  x.lstrip('raw_mtu=langEnglishnonefreqdistHz').rstrip('=Hz'))
-        df2['time'] = df2['time'].map(lambda x:  x.lstrip('').rstrip('mod=mtu,cli=mod=syn+ackmod=uptimemod=httpreqmod= httpmod=host chang'))
-        df2_checkpub = df2['client_ip'].apply(lambda x: ipaddress.ip_address(x).is_global)
-        df2_checkpriv = df2['client_ip'].apply(lambda x: ipaddress.ip_address(x).is_private)
-        return (df2)
     #def logwrite():
         #df2 = df2.to_csv('p0f.csv', index=None, encoding='utf-8')
